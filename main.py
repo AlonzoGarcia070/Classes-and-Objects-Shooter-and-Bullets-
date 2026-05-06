9d4535ed558bb64cabc23b8889aa841cfab254c6
AlonzoGarcia070 <alonzoagarcia73@gmail.com>
2026-05-06 13:09:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -81,77 +81,36 @@ p2 = Player(100,0,"blue",screen,"Right","Left", "Up")
 while p1.alive and p2.alive:
     p1.move()
     p2.move()
-<<<<<<< HEAD
     for bullet in p1.bullets:
         bullet.move()
-        if bullet.distance(p2)>20:
-            pass
-        elif bullet.distance(p2)<20:
+        if bullet.distance(p2)<20:
             bullet.hideturtle()
             p1.bullets.remove(bullet)
             p2.health-=1
-        elif p2.health==2:
-            p2.color("yellow")
-        elif p2.health==1:
-            p2.color("red")
-        else:
-            alive=False
-            p2.hideturtle()
+            print(p2.health)
+            if p2.health==2:
+                p2.color("yellow")
+            elif p2.health==1:
+                p2.color("red")
+            else:
+                p2.alive=False
+                p2.hideturtle()
 
     for bullet in p2.bullets:
         bullet.move()
-        if bullet.distance(p1)>20:
-            pass
-        elif bullet.distance(p1)<20:
+        if bullet.distance(p1)<20:
             bullet.hideturtle()
             p2.bullets.remove(bullet)
             p1.health-=1
-        elif p1.health==2:
-            p1.color("yellow")
-        elif p1.health==1:
-            p1.color("red")
-        else:
-            alive=False
-            p1.hideturtle()
+            print(p1.health)
+            if p1.health==2:
+                p1.color("yellow")
+            elif p1.health==1:
+                p1.color("red")
+            else:
+                p1.alive=False
+                p1.hideturtle()
            
-            # if player.health==3:
-            #     turtle.color(player.color)
-            # elif player.health==2 and player.distance(bullet) < 20:
-            #     turtle.color("yellow")
-            #     turtle.health-=1
-            # elif player.health==2 and player.distance(bullet) < 20:
-            #     turtle.color("red")
-            #     turtle.health-=1
-           
-=======
 
-    for obj in turtles:
-        move_heading(obj, turtles)
-        if player!= None and player.distance(obj) < 20:
-            obj.hideturtle()
-            turtles.remove(obj)
-            turtle.health-=1
-
-    for bullet in turtles:
-        move_heading(bullet, turtles)
-        if player.health==3:
-            turtle.color(player.color)
-        if player.health==2 and player.distance(bullet) < 20:
-            turtle.color("yellow")
-        elif player.health==1 and player.distance(bullet) < 20:
-            turtle.color("red")
-        else:
-            alive=False
-            obj.hideturtle()
-            turtles.remove(bullet)
->>>>>>> 6a0766269d674b2a7ebcf40851520f5a5a1b6296
-
-# def detect_bullets(p1,p2):
-#     if p1!= None and bullets.distance(obj)<20:
-#         p1.color("yellow") 
-#     elif p1.distance==bullets.distance and p1.color==yellow:
-#         p1.color("red")
-#     else:
-# if
 
 screen.exitonclick()
